d1f801cc53b8c016acbbe7e712465b3733c8273e
Bruno Henrique D. Seco <bruuno14@msn.com>
2017-02-06 20:54:03 -0200

    Add files via upload

---
+++ indexedDB.js
@@ -0,0 +1,120 @@
+﻿
+angular.module('common.utils')
+    .service('DbIndexed', DbIndexed);
+
+function DbIndexed($timeout) {
+
+    var init = function () {
+
+        this.Get = _get;
+        this.Add = _add;
+        this.Update = _update;
+        this.Remove = _remove;
+        this.CreateDB = _createdb;
+
+        this.key = 0;
+        this.data = {};
+
+        this.SuccessHandle = function (data) { return data; };
+        this.ErrorHandle = function (data) { return data; };
+
+        this.result = {};
+
+        this.objectStore = null;
+
+        this.DbVersion = 1;
+        this.DbName = null;
+        this.Table = null;
+        this.KeyPath = null;
+
+        var self = this;
+
+        function _get(chave) {
+
+            _getDB().onsuccess = function (event) {
+
+                var db = event.target.result;
+                var transection = db.transaction([self.Table]);
+                var objectStore = transection.objectStore(self.Table);
+
+                var requestGet = objectStore.get(chave);
+
+                requestGet.onsuccess = function (e) {
+                    $timeout(function () {
+                        return self.SuccessHandle(requestGet.result)
+                    }, 1);
+                };
+
+                requestGet.onerror = function (e) {
+                    return self.ErrorHandle(e)
+                }
+            };
+        };
+
+        function _add(item) {
+            _getDB().onsuccess = function (event) {
+                var db = event.target.result;
+                var transection = db.transaction([self.Table], "readwrite");
+                var objectStore = transection.objectStore(self.Table);
+                objectStore.add(item);
+            }
+        };
+
+        function _update() {
+            _getDB().onsuccess = function (event) {
+                var db = event.target.result;
+                var transection = db.transaction([self.Table], "readwrite");
+                var objectStore = transection.objectStore(self.Table);
+                var requestUpdate = objectStore.get(self.key);
+
+                requestUpdate.onsuccess = function (event) {
+                    var dados = this.result;
+                    dados = self.data;
+                    var requestUpdate = objectStore.put(dados);
+                };
+            }
+        };
+
+        function _remove() {
+            _getDB().onsuccess = function (event) {
+                var db = event.target.result;
+                var transection = db.transaction([self.Table], "readwrite");
+                var objectStore = transection.objectStore(self.Table);
+                objectStore.delete(self.key);
+            }
+        };
+
+        function _createdb() {
+            var response = indexedDB.open(self.DbName, self.DbVersion);
+            response.onerror = function (event) {
+            };
+
+            response.onsuccess = function (event) {
+
+            };
+
+            response.onupgradeneeded = function (event) {
+                var db = event.target.result;
+
+                if (!self.KeyPath)
+                    throw "KeyPath não definido";
+
+                var objectStore = db.createObjectStore(self.Table, { keyPath: self.KeyPath });
+
+                objectStore.transaction.oncomplete = function (event) {
+
+                }
+            };
+        };
+
+        function _getDB() {
+            return indexedDB.open(self.DbName, self.DbVersion);
+        }
+    };
+
+    this.start = function () {
+        return new init();
+    }
+
+
+};
---
+++ uri.js
@@ -0,0 +1,76 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('common.utils')
+        .service('Uri', Uri);
+
+    Uri.$inject = ['$httpParamSerializer', 'endpoints']
+
+    function Uri($httpParamSerializer, endpoints) {
+
+        var init = function (resource) {
+
+            this.Resourse = resource;
+            
+            this.DefaultFilter = {
+                PageSize: 50,
+                PageIndex: 0,
+                IsPaginate: true,
+                QueryOptimizerBehavior: "",
+            };
+
+            this.queryStringFilter = _queryStringFilter;
+            this.makeGetBaseUrl = _makeGetBaseUrl;
+            this.makeUri = _makeUri;
+            this.setEndPoint = _setEndPoint;
+
+            var self = this;
+
+            function _setEndPoint(endpoint) {
+                this.EndPoint = endpoint;
+            }
+
+            function _queryStringFilter(filter, filterBehavior) {
+
+                if (filter != undefined && filter.OrderFields !== undefined) {
+                    filter.IsOrderByDynamic = true;
+                    if (filter.OrderByType === undefined)
+                        filter.OrderByType = 1;
+                }
+
+                if (filterBehavior !== undefined)
+                    filter.FilterBehavior = filterBehavior;
+
+                var filterMerged = $httpParamSerializer(angular.merge({}, self.DefaultFilter, filter));
+
+                if (filter.Id !== undefined)
+                    return String.format("{0}?{1}", filter.Id, filterMerged);
+
+                return String.format("?{0}", filterMerged);
+            }
+
+            function _makeUri() {
+                return String.format("{0}/{1}", _makeEndPoint(), self.Resourse)
+            }
+
+            function _makeEndPoint() {
+                if (!self.EndPoint)
+                    return endpoints.DEFAULT;
+
+                return endpoints[self.EndPoint];
+            }
+
+
+            function _makeGetBaseUrl(filter) {
+                return String.format("{0}/{1}", _makeUri(), _queryStringFilter(filter));
+            }
+
+        }
+
+        this.resource = function (resource, endpoint) {
+            return new init(resource, endpoint);
+        }
+
+    }
+
+})();
